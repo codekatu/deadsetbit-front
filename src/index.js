5d78a835284a7fa83c8ba6e7611c7c16d4d56393
joni <jonipharju@icloud.com>
2023-04-11 13:07:31 +0300

    webpack css testing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-import "./reset.css";
-import "./style.css";
-console.log("hei");
+// import "./reset.css";
+// import "./style.css";
+require("./reset.css");
+require("./style.css");
+
+console.log("heippa");
