0b58328f4abdb93df99794a24a058cc01a345016
joni <jonipharju@icloud.com>
2023-05-17 09:18:31 +0300

    navbar bugs fixed, banzaiman tab selector made better, dog eyes now get the angle from each eye rather than from the middle of the dog, many small fixes(image aspect ratios etc)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+// import css files
 import "./reset.css";
 import "./style.css";
 
-// getting dom elements via id
+// getting dom elements
+const pageWrapper = document.getElementById("pageWrapper");
 const navbar = document.getElementById("navbar");
+const listItems = document.getElementsByClassName("listItem");
 const logoContainer = document.getElementById("logoContainer");
 const logo = document.getElementById("logoSvg");
+const leftEye = document.getElementById("leftEye");
+const rightEye = document.getElementById("rightEye");
 const eyes = document.querySelectorAll(".eye");
 const doggo = document.getElementById("doggo");
 const tabImage = document.getElementById("banzaimanGameImage");
@@ -16,7 +21,9 @@ const maracasButton = document.getElementById("maracasButton");
 const menuButton = document.getElementById("mainMenu");
 const navbarButton = document.getElementById("navbarButton");
 const tabButtons = document.getElementsByClassName("tabSelectorButton");
+const buttonImages = document.getElementsByClassName("buttonImage");
 const mainMenuListLinks = document.getElementsByClassName("mainMenuListLink");
+
 // adds event listeners to tab selector buttons on load
 window.onload = function () {
   for (let index = 0; index < tabButtons.length; index++) {
@@ -24,139 +31,117 @@ window.onload = function () {
     element.addEventListener("click", (e) => {
       tabButton(e);
     });
-    navbarButton.addEventListener("click", menuOpenClose);
   }
+
+  for (let index = 0; index < mainMenuListLinks.length; index++) {
+    const element = mainMenuListLinks[index];
+    element.addEventListener("click", (e) => {
+      menuButton.style.display = "none";
+    });
+  }
+
+  navbarButton.addEventListener("click", menuOpenClose);
 
   for (let index = 0; index < mainMenuListLinks.length; index++) {
     const element = mainMenuListLinks[index];
   }
 };
 
-// function that checks which tab button was pressed, then updates the game image to the corresponding image
-// also updates the colors of the tab selector buttons
+// function that changes the game picture depending on which button is clicked. Also calls the changeButtonColor function to change the color of the button
 function tabButton(e) {
-  if (e.target.id === "gongButton") {
+  if (e.target.id === "gongButton" || e.target.id === "gongButtonImage") {
     tabImage.setAttribute(
       "src",
       "assets/banzaimanSection/tabSelector/banzai_1.png"
     );
-
-    gongButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/gongWhite.svg)";
-
-    mountainButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/mountain.svg)";
-
-    glyphButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/glyph.svg)";
-
-    spaceButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/space.svg)";
-
-    maracasButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/maracas.svg)";
-  } else if (e.target.id === "mountainButton") {
+    changeButtonColor("gongButtonImage");
+  } else if (
+    e.target.id === "mountainButton" ||
+    e.target.id === "mountainButtonImage"
+  ) {
     tabImage.setAttribute(
       "src",
       "assets/banzaimanSection/tabSelector/banzai_2.png"
     );
-
-    mountainButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/mountainWhite.svg)";
-
-    gongButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/gong.svg)";
-
-    glyphButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/glyph.svg)";
-
-    spaceButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/space.svg)";
-
-    maracasButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/maracas.svg)";
-  } else if (e.target.id === "glyphButton") {
+    changeButtonColor("mountainButtonImage");
+  } else if (
+    e.target.id === "glyphButton" ||
+    e.target.id === "glyphButtonImage"
+  ) {
     tabImage.setAttribute(
       "src",
       "assets/banzaimanSection/tabSelector/banzai_3.png"
     );
-
-    glyphButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/glyphWhite.svg)";
-
-    mountainButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/mountain.svg)";
-
-    gongButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/gong.svg)";
-
-    spaceButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/space.svg)";
-
-    maracasButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/maracas.svg)";
-  } else if (e.target.id === "spaceButton") {
+    changeButtonColor("glyphButtonImage");
+  } else if (
+    e.target.id === "spaceButton" ||
+    e.target.id === "spaceButtonImage"
+  ) {
     tabImage.setAttribute(
       "src",
       "assets/banzaimanSection/tabSelector/banzai_4.png"
     );
-
-    spaceButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/spaceWhite.svg)";
-
-    glyphButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/glyph.svg)";
-
-    mountainButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/mountain.svg)";
-
-    gongButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/gong.svg)";
-
-    maracasButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/maracas.svg)";
-  } else if (e.target.id === "maracasButton") {
+    changeButtonColor("spaceButtonImage");
+  } else if (
+    e.target.id === "maracasButton" ||
+    e.target.id === "maracasButtonImage"
+  ) {
     tabImage.setAttribute(
       "src",
       "assets/banzaimanSection/tabSelector/banzai_5.png"
     );
-    maracasButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/maracasWhite.svg)";
-
-    spaceButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/space.svg)";
-
-    glyphButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/glyph.svg)";
-
-    mountainButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/mountain.svg)";
-
-    gongButton.style.backgroundImage =
-      "url(assets/banzaimanSection/tabSelector/gong.svg)";
+    changeButtonColor("maracasButtonImage");
   }
 }
 
-// scroll event listener to change navbar
+// function that changes the button color
+function changeButtonColor(buttonImageId) {
+  for (let index = 0; index < buttonImages.length; index++) {
+    const element = buttonImages[index];
+
+    if (buttonImageId === element.id) {
+      element.classList.add("buttonImageSelected");
+    } else if (buttonImageId !== element.id) {
+      element.classList.remove("buttonImageSelected");
+    }
+  }
+}
+
+// scroll event listener to call navbarScrollReponsive function which changes the navbar on scroll
 window.addEventListener("scroll", navbarScrollResponsive);
 
+// function that makes the dogs eyes follow the cursor
+// code that is commented out counts the angle from the middle of the dogs container so that both eyes follow the cursor with same angle, therefore not crossing when hovering between the eyes
 window.addEventListener("mousemove", (e) => {
-  const rekt = doggo.getBoundingClientRect(); // get the dimensions and location relative to viewport for the dog image
+  // const rekt = doggo.getBoundingClientRect();
 
-  // coordinates for the middle of the anchor image
-  const doggoX = rekt.left + rekt.width / 2;
-  const doggoY = rekt.top + rekt.height / 2;
+  // const doggoX = rekt.left + rekt.width / 2;
+  // const doggoY = rekt.top + rekt.height / 2;
+
+  const rektLeft = leftEye.getBoundingClientRect(); // get the dimensions and location relative to viewport for the left eye
+  const rektRight = rightEye.getBoundingClientRect(); // get the dimensions and location relative to viewport for the right eye
+
+  const leftEyeX = rektLeft.left + rektLeft.width / 2;
+  const leftEyeY = rektLeft.top + rektLeft.width / 2;
+
+  const rightEyeX = rektRight.left + rektRight.width / 2;
+  const rightEyeY = rektRight.top + rektRight.width / 2;
 
   // position of the cursor
   const mouseX = e.clientX;
   const mouseY = e.clientY;
 
-  const angleDeg = angle(mouseX, mouseY, doggoX, doggoY);
-  console.log(angleDeg);
+  // const angleDeg = angle(mouseX, mouseY, doggoX, doggoY);
 
-  eyes.forEach((eye) => {
-    eye.style.transform = `rotate(${90 + angleDeg}deg)`;
-  });
+  // eyes.forEach((eye) => {
+  //   eye.style.transform = `rotate(${90 + angleDeg}deg)`;
+  // });
+
+  const angleDegLeftEye = angle(mouseX, mouseY, leftEyeX, leftEyeY); // get the degree of the angle between mouse and the eye
+  const angleDegRightEye = angle(mouseX, mouseY, rightEyeX, rightEyeY); // get the degree of the angle between mouse and the eye
+
+  leftEye.style.transform = `rotate(${90 + angleDegLeftEye}deg)`;
+  rightEye.style.transform = `rotate(${90 + angleDegRightEye}deg)`;
 });
 
 //takes mouse position and dog position as arguments to count the angle between them
@@ -167,12 +152,9 @@ function angle(cx, cy, ex, ey) {
   const deg = (rad * 180) / Math.PI; // converts the radian into degrees
   return deg;
 }
+
 // function that changes the navbar on depending on scrollY position
 function navbarScrollResponsive() {
-  // getting HTMLcollections from dom
-  const listItems = document.getElementsByClassName("listItem");
-  const listItemsScrolled = document.getElementsByClassName("listItemScrolled");
-
   // when scroll goes over 150 add scrolled class to these elements
   if (window.scrollY > 100) {
     navbar.classList.add("navbarScrolled");
@@ -193,16 +175,18 @@ function navbarScrollResponsive() {
     logo.classList.remove("logoSvgScrolled");
 
     // loop trough the listItemsScrolled HTMLcollection and change their className
-    for (let index = 0; index < listItemsScrolled.length; index++) {
-      const element = listItemsScrolled[index];
+    for (let index = 0; index < listItems.length; index++) {
+      const element = listItems[index];
       element.classList.remove("listItemScrolled");
     }
   }
 }
 
+// function that opens and closes the menu
 function menuOpenClose() {
   if (menuButton.style.display === "flex") {
     menuButton.style.display = "none";
+    pageWrapper.classList.toggle = "mainMenuBackgroundColor";
   } else {
     menuButton.style.display = "flex";
   }
