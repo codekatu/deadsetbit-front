141f0a0997e421367dc1b8b94e07c3f594af15e5
joni <jonipharju@icloud.com>
2023-05-19 16:06:21 +0300

    fixed hero section on mobile, removed space above employee section header on mobile, made the hamburger menu opacity layer and added click listener to it to to close the menu, made hamburger menu button animation

--- src/index.js
+++ src/index.js
@@ -4,51 +4,71 @@ import "./style.css";
 import "./fonts.css";
 
 // getting dom elements
-const pageWrapper = document.getElementById("pageWrapper");
+// const pageWrapper = document.getElementById("pageWrapper");
 const navbar = document.getElementById("navbar");
 const listItems = document.getElementsByClassName("listItem");
 const logoContainer = document.getElementById("logoContainer");
 const logo = document.getElementById("logoSvg");
 const leftEye = document.getElementById("leftEye");
 const rightEye = document.getElementById("rightEye");
-const eyes = document.querySelectorAll(".eye");
-const doggo = document.getElementById("doggo");
+const eyes = document.querySelectorAll(".eye"); // needed if using the other variant of the dogEyeMove
+const doggo = document.getElementById("doggo"); // needed if using the other variant of the dogEyeMove
 const tabImage = document.getElementById("banzaimanGameImage");
-const gongButton = document.getElementById("gongButton");
-const mountainButton = document.getElementById("mountainButton");
-const glyphButton = document.getElementById("glyphButton");
-const spaceButton = document.getElementById("spaceButton");
-const maracasButton = document.getElementById("maracasButton");
-const menuButton = document.getElementById("mainMenu");
+// const gongButton = document.getElementById("gongButton");
+// const mountainButton = document.getElementById("mountainButton");
+// const glyphButton = document.getElementById("glyphButton");
+// const spaceButton = document.getElementById("spaceButton");
+// const maracasButton = document.getElementById("maracasButton");
+// const menuButton = document.getElementById("mainMenu");
 const navbarButton = document.getElementById("navbarButton");
 const tabButtons = document.getElementsByClassName("tabSelectorButton");
 const buttonImages = document.getElementsByClassName("buttonImage");
 const mainMenuListLinks = document.getElementsByClassName("mainMenuListLink");
+const mainMenuOpacityLayer = document.getElementById("mainMenuOpacityLayer");
+const mainMenuContainer = document.getElementById("mainMenuContainer");
 
 // adds event listeners to tab selector buttons on load
 window.onload = function () {
   for (let index = 0; index < tabButtons.length; index++) {
     const element = tabButtons[index];
-    element.addEventListener("click", (e) => {
-      tabButton(e);
-    });
+    element.addEventListener("click", tabButton);
   }
 
   for (let index = 0; index < mainMenuListLinks.length; index++) {
     const element = mainMenuListLinks[index];
-    element.addEventListener("click", (e) => {
-      menuButton.style.display = "none";
-    });
+    // element.addEventListener("click", (e) => {
+    //   menuButton.style.display = "none";
+    // });
+    element.addEventListener("click", menuOpenClose);
   }
 
   navbarButton.addEventListener("click", menuOpenClose);
-
-  for (let index = 0; index < mainMenuListLinks.length; index++) {
-    const element = mainMenuListLinks[index];
-  }
+  mainMenuOpacityLayer.addEventListener("click", menuOpenClose);
 
   // page starts with the gong image selected so we make it's color white onload
   changeButtonColor("gongButtonImage");
+};
+
+// throttle function to limit the amount of times a function is called
+const throttle = (fn, wait) => {
+  let inThrottle, lastFn, lastTime;
+  return function () {
+    const context = this,
+      args = arguments;
+    if (!inThrottle) {
+      fn.apply(context, args);
+      lastTime = Date.now();
+      inThrottle = true;
+    } else {
+      clearTimeout(lastFn);
+      lastFn = setTimeout(function () {
+        if (Date.now() - lastTime >= wait) {
+          fn.apply(context, args);
+          lastTime = Date.now();
+        }
+      }, Math.max(wait - (Date.now() - lastTime), 0));
+    }
+  };
 };
 
 // function that changes the game picture depending on which button is clicked. Also calls the changeButtonColor function to change the color of the button
@@ -111,39 +131,13 @@ function changeButtonColor(buttonImageId) {
   }
 }
 
-// scroll event listener to call navbarScrollReponsive function which changes the navbar on scroll
-window.addEventListener("scroll", navbarScrollResponsive);
-
-const throttle = (fn, wait) => {
-  let inThrottle, lastFn, lastTime;
-  return function () {
-    const context = this,
-      args = arguments;
-    if (!inThrottle) {
-      fn.apply(context, args);
-      lastTime = Date.now();
-      inThrottle = true;
-    } else {
-      clearTimeout(lastFn);
-      lastFn = setTimeout(function () {
-        if (Date.now() - lastTime >= wait) {
-          fn.apply(context, args);
-          lastTime = Date.now();
-        }
-      }, Math.max(wait - (Date.now() - lastTime), 0));
-    }
-  };
-};
 // function that makes the dogs eyes follow the cursor
 // code that is commented out counts the angle from the middle of the dogs container so that both eyes follow the cursor with same angle, therefore not crossing when hovering between the eyes
-window.addEventListener("mousemove", throttle(dogEyeMove, 25));
-
 function dogEyeMove(e) {
   // const rekt = doggo.getBoundingClientRect();
 
   // const doggoX = rekt.left + rekt.width / 2;
   // const doggoY = rekt.top + rekt.height / 2;
-  console.log("hello world");
   const rektLeft = leftEye.getBoundingClientRect(); // get the dimensions and location relative to viewport for the left eye
   const rektRight = rightEye.getBoundingClientRect(); // get the dimensions and location relative to viewport for the right eye
 
@@ -170,7 +164,7 @@ function dogEyeMove(e) {
   rightEye.style.transform = `rotate(${90 + angleDegRightEye}deg)`;
 }
 
-//takes mouse position and dog position as arguments to count the angle between them
+//takes mouse position and dodgs eye position as arguments to count the angle between them
 function angle(cx, cy, ex, ey) {
   const dy = ey - cy;
   const dx = ex - cx;
@@ -210,10 +204,16 @@ function navbarScrollResponsive() {
 
 // function that opens and closes the menu
 function menuOpenClose() {
-  if (menuButton.style.display === "flex") {
-    menuButton.style.display = "none";
-    pageWrapper.classList.toggle = "mainMenuBackgroundColor";
+  if (mainMenuContainer.style.display === "flex") {
+    mainMenuContainer.style.display = "none";
+    navbarButton.classList.toggle("clicked");
   } else {
-    menuButton.style.display = "flex";
+    mainMenuContainer.style.display = "flex";
+    navbarButton.classList.toggle("clicked");
   }
 }
+
+// scroll event listener to call navbarScrollReponsive function which changes the navbar on scroll
+window.addEventListener("scroll", navbarScrollResponsive);
+// adds event listener to mouse move that calls throttle function with dogEyeMove function and a timeout that limits the amount of times the function is called
+window.addEventListener("mousemove", throttle(dogEyeMove, 25));
